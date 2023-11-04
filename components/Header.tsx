93d4d0f59f6a453fdca3c64fcb1c07c781fccc25
Jichul54 <kjc778869@gmail.com>
2023-11-04 22:06:41 +0900

    Reorganize App directory and create components folder

---
+++ components/Header.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Header: React.FC = () => {
+  return (
+    <header>
+      <div className="header-box">
+        <h1>Design thinking engineering project Group: 5</h1>
+      </div>
+      <div className="top-box">
+        <h2>Bodhi</h2>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
